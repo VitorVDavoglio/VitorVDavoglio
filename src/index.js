1fea482b7a3cbcc9d485252b99f31aa97ccf82bd
VitorVDavoglio <vieiradavoglio@gmail.com>
2024-08-24 19:25:41 -0300

    inicio da pagina

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import "./style/global.css";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <h1>Meu site e meu portifólio</h1>
+);
